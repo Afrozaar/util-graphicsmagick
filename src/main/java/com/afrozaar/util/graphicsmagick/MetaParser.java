1b630cad6df1f098357d75875f9c6bc70f939661
Johan Mynhardt <johanmynhardt@gmail.com>
2016-03-07 12:32:51 +0200

    BAOBAB-1580: Refactor parsing to be cleaner, handle rogue data lines.

--- src/main/java/com/afrozaar/util/graphicsmagick/MetaParser.java
+++ src/main/java/com/afrozaar/util/graphicsmagick/MetaParser.java
@@ -4,15 +4,21 @@ import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Sets;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.AbstractMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
 public class MetaParser {
+
+    static final Logger LOG = LoggerFactory.getLogger(MetaParser.class);
 
     public static Optional<Map<String, Object>> parseResult(String execute) {
 
@@ -22,29 +28,45 @@ public class MetaParser {
 
         Meta last = null;
 
-        for (String line : lines) {
-            Optional<Meta> entryOpt = parseEntry(line);
-            if (entryOpt.isPresent()) {
-                final Meta entry = entryOpt.get();
+        for (final String line : lines) {
+            if (isEmptyLine(line)) {
+                LOG.warn("Empty line, skipping processing.");
+                continue;
+            }
+
+            final String[] split = line.split(":", 2);
+            final boolean isPossibleRogueDataLine = isPossibleRogueDataLine(split);
+
+            if (Objects.nonNull(last) && (isNonKeyValueEntry(split) || isPossibleRogueDataLine)) {
+                final String newValue = isPossibleRogueDataLine ? line.trim() : split[0].trim();
+                LOG.debug("Updating last data key={} value={}", last.data.getKey(), newValue);
+                last.data = new AbstractMap.SimpleImmutableEntry<>(last.data.getKey(), newValue);
+            } else {
+                final Meta entry = Meta.of(split);
                 if (last == null) {
                     root = last = entry;
                 } else {
-                    if (entry.depth == last.depth) {
-                        entry.parent = last.parent;
-                        last.parent.children.add(entry);
-                    } else if (entry.depth > last.depth) {
-                        entry.parent = last;
-                        last.children.add(entry);
-                    } else if (entry.depth < last.depth) {
-                        entry.parent = findParent(entry.depth, last);
-                        entry.parent.children.add(entry);
-                    }
+                    entry.parent = findParentFromDepth(entry, last);
+                    entry.parent.addChild(entry);
+
                     last = entry;
                 }
             }
         }
 
         return Optional.of(ImmutableMap.of("data", root));
+    }
+
+    private static Meta findParentFromDepth(Meta entry, Meta last) {
+        if (entry.depth == last.depth) {
+            return last.parent;
+        } else if (entry.depth > last.depth) {
+            return last;
+        } else if (entry.depth < last.depth) {
+            return findParent(entry.depth, last);
+        } else {
+            throw new IllegalStateException();
+        }
     }
 
     private static Meta findParent(int depth, Meta last) {
@@ -63,32 +85,66 @@ public class MetaParser {
         }
     }
 
-    static Optional<Meta> parseEntry(String line) {
-        final Logger logger = LoggerFactory.getLogger(MetaParser.class);
-        logger.info("Parsing meta from line: {}", line);
-        if (Strings.isNullOrEmpty(line) || line.trim().isEmpty()) {
-            logger.warn("Skipping empty line.");
-            return Optional.empty();
-        }
-        try {
-            final String[] split = line.split(":", 2);
-            Meta newMeta = new Meta();
-            newMeta.data = new AbstractMap.SimpleImmutableEntry<>(split[0].trim(), split[1]);
-            newMeta.depth = indent(0, split[0]);
-            return Optional.of(newMeta);
-        } catch (Exception e) {
-            logger.error("Skipping line: '{}'. Error ", line, e);
-            return Optional.empty();
+    private static int charCount(final int count, final char chr, final int index, final String input) {
+        if (index >= input.length()) {
+            return count;
+        } else {
+            int newCount = input.charAt(index) == chr ? count + 1 : count;
+            return charCount(newCount, chr, index+1, input);
         }
     }
 
+    private static boolean isEmptyLine(String line) {
+        return Strings.isNullOrEmpty(line) || line.trim().isEmpty();
+    }
+
+    private static boolean isNonKeyValueEntry(String[] split) {
+        return split.length < 2;
+    }
+
+    /**
+     * A possible rogue line contains a ':' which is not a field separator, eg.: 'This is some random text that belongs to a previous entry: noted?'
+     * Also, the indentation is broken. At this stage it is not certain whether it is a data entry issue onto the meta entry or not.
+     * Currently it is hard to tell the conditions would indicate a rogue line, but a good guess is that anything NOT matching [\w\d\:\-] can indicate this.
+     */
+    private static boolean isPossibleRogueDataLine(String[] split) {
+
+        //char[] rogueChars = {'.', '(', ')'};
+        final String regex = "(\\w|\\d|\\s|\\-)+";
+        return !split[0].matches(regex);
+
+        //return split[0].matches("[^\\w\\d:\\-]+");
+
+        //return charCount(0, '.', 0, split[0].trim()) > 0;
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     static class Meta {
         Meta parent;
         Map.Entry data;
         int depth = 0;
         Set<Meta> children = Sets.newLinkedHashSet();
 
-        public Meta() {
+        Meta() {
+        }
+
+        Meta(String key, String value) {
+            this.data = new AbstractMap.SimpleImmutableEntry<>(key, value);
+        }
+
+        static Meta of(String key, String value) {
+            return new Meta(key, value);
+        }
+
+        static Meta of(String[] split) {
+            Meta newMeta = Meta.of(split[0].trim(), split[1].trim());
+            newMeta.depth = indent(0, split[0]);
+            return newMeta;
+        }
+
+        Meta addChild(Meta child) {
+            children.add(child);
+            return this;
         }
 
         public Set<Meta> getChildren() {
@@ -99,6 +155,7 @@ public class MetaParser {
             return data;
         }
 
+        @JsonIgnore
         public int getDepth() {
             return depth;
         }
--- src/test/java/com/afrozaar/util/graphicsmagick/MetaParserTest.java
+++ src/test/java/com/afrozaar/util/graphicsmagick/MetaParserTest.java
@@ -2,25 +2,30 @@ package com.afrozaar.util.graphicsmagick;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import static org.junit.Assert.fail;
+
 import com.google.common.io.ByteSource;
 
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
+import java.util.Optional;
 
 public class MetaParserTest {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MetaParserTest.class);
+
     @Test
     public void parseResult() throws Exception {
-        ByteSource byteSource = new ByteSource() {
-            @Override
-            public InputStream openStream() throws IOException {
-                return MetaParserTest.class.getResourceAsStream("/imageinfo.txt");
-            }
-        };
-
-        MetaParser.parseResult(new String(byteSource.read()));
+        final String context = "/imageinfo.txt";
+        final Optional<Map<String, Object>> stringObjectMap = MetaParser.parseResult(readOutput(context));
     }
 
     @Test
@@ -31,5 +36,37 @@ public class MetaParserTest {
 
         int result2 = MetaParser.indent(0, "    this has one indent");
         assertThat(result2).isEqualTo(4);
+    }
+
+    @Test
+    public void ParseWeirdIndents() throws IOException {
+        final String context = "/rugbymetadata.txt";
+        final Optional<Map<String, Object>> result = MetaParser.parseResult(readOutput(context));
+
+        if (result.isPresent()) {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writer(new DefaultPrettyPrinter()).writeValue(System.out, result.get().get("data"));
+        } else {
+            fail();
+        }
+    }
+
+    @Test
+    public void RogueLineData() {
+
+        final String input = "this or. that";
+        final String input2 = "Profile-1";
+        final String regex = "(\\w|\\d|\\s|\\-)+";
+        LOG.info("'{}'.matches({}): {}", input, regex, input.matches(regex));
+        LOG.info("'{}'.matches({}): {}", input2, regex, input2.matches(regex));
+    }
+
+    private String readOutput(final String context) throws IOException {
+        return new String(new ByteSource() {
+            @Override
+            public InputStream openStream() throws IOException {
+                return MetaParserTest.class.getResourceAsStream(context);
+            }
+        }.read());
     }
 }
