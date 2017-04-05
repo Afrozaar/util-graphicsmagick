6a3ca0a0d4baae0b98479b5d7fdf14253d2552a8
Johan Mynhardt <johanmynhardt@gmail.com>
2017-04-05 13:15:22 +0200

    Fix for BAOBAB-2401: Only retrieve the extension from the URL filename minus the query parameters.

---
+++ src/test/java/com/afrozaar/util/graphicsmagick/AbstractImageIOTest.java
@@ -0,0 +1,19 @@
+package com.afrozaar.util.graphicsmagick;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class AbstractImageIOTest {
+    AbstractImageIO imageIO = new GraphicsMagicImageIO();
+
+    @Test
+    public void testGetExtensionFromSourceWithQueryParameters_MustNotHaveTheQueryParameters() {
+        final String source = "https://inm-baobab-test-eu-west-1.s3-eu-west-1.amazonaws.com/private/inm/caa/11554900/model/0/200/CA_NWS_E1_160117_p01.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20170405T083604Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3599&X-Amz-Credential=AKIAINMXWPFHXVF2RJZQ%2F20170405%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Signature=c40e0f61c7604a667ab2eea48b9077edf976cafae4e771c176de18f01487778e";
+
+        final String extension = imageIO.getExtension(source);
+
+        Assertions.assertThat(extension).doesNotContain("X-Amz-Algorithm").isEqualTo(".pdf");
+
+    }
+
+}
